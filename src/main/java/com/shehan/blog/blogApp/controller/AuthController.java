d9e3fee0e61628e97ec39ff233206437333345a2
Shehan Karunarathna <shehank@atlinkcom.com>
2022-05-24 09:48:55 +0530

    add signin commit

--- src/main/java/com/shehan/blog/blogApp/controller/AuthController.java
+++ src/main/java/com/shehan/blog/blogApp/controller/AuthController.java
@@ -1,17 +1,27 @@
 package com.shehan.blog.blogApp.controller;
 
+import com.shehan.blog.blogApp.model.Role;
+import com.shehan.blog.blogApp.model.SignupDto;
+import com.shehan.blog.blogApp.model.User;
 import com.shehan.blog.blogApp.payload.LoginDto;
+import com.shehan.blog.blogApp.repository.RoleRepository;
+import com.shehan.blog.blogApp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,10 +30,40 @@ public class AuthController {
     @Autowired
     AuthenticationManager authenticationManager;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @PostMapping("/signin")
     public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto){
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsernameOrEmail(), loginDto.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return  new ResponseEntity<>("User sign in successfully", HttpStatus.OK);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> registerUser(@RequestBody SignupDto signupDto){
+        if(userRepository.existsByUsername(signupDto.getUsername())){
+            return  new ResponseEntity<>("Username is already taken", HttpStatus.BAD_REQUEST);
+        }
+        if(userRepository.existsByEmail(signupDto.getEmail())){
+            return  new ResponseEntity<>("Email is already taken", HttpStatus.BAD_REQUEST);
+        }
+
+        User user = new User();
+        user.setName(signupDto.getName());
+        user.setUsername(signupDto.getUsername());
+        user.setPassword(signupDto.getPassword());
+        user.setEmail(signupDto.getEmail());
+
+        Role roles = roleRepository.findByName("ROLW_ADMIN").get();
+        user.setRoles(Collections.singleton(roles));
+        userRepository.save(user);
+        return new ResponseEntity<String >("User is registered successfully", HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/shehan/blog/blogApp/model/SignupDto.java
@@ -0,0 +1,40 @@
+package com.shehan.blog.blogApp.model;
+
+public class SignupDto {
+    private String name;
+    private String email;
+    private String password;
+    private  String username;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
